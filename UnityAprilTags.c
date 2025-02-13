41404a744d7de21e00948a0e6b467d5fefb2753d
Ian Thompson <ian@yann.io>
2025-02-12 23:12:34 -0800

    Initial PoC for creating the wrapper library and linking it to upstream apriltags

---
+++ UnityAprilTags.c
@@ -0,0 +1,25 @@
+﻿// UnityAprilTags.cpp : Defines the entry point for the application.
+//
+
+#include "UnityAprilTags.h"
+
+#include <apriltag.h>
+#include <tag36h11.h>
+
+#include <stdio.h>
+
+#if defined(_WIN32) || defined(WIN32)
+#define EXPORTED_API __declspec(dllexport)
+#else
+#define EXPORTED_API
+#endif
+
+EXPORTED_API void do_stuff() { printf("Doing stuff\n"); }
+
+EXPORTED_API void do_apriltags() {
+  apriltag_detector_t* detector = apriltag_detector_create();
+  apriltag_family_t* family = tag36h11_create();
+  apriltag_detector_add_family(detector, family);
+  apriltag_detector_destroy(detector);
+  tag36h11_destroy(family);
+}
